500d8e71ca75ed671e5bec0580ed66554c4db1f5
Raven356 <matroskin7777@gmail.com>
2023-12-06 23:04:48 +0200

    added tokens

--- PAEG6/PAEG6/Program.cs
+++ PAEG6/PAEG6/Program.cs
@@ -20,26 +20,33 @@ class Program
 
         BBS bBS = new BBS();
         var bbsKeys = bBS.GenerateKeys();
+
+        List<Token> tokens = new List<Token>();
+        for (int i = 0; i < amount; i++)
+        {
+            tokens.Add(new Token { Id = ids[i], Keys = keys[i], N = bbsKeys.Item1 });
+        }
+
         List<Bulletein> bulleteins = new List<Bulletein>();
         List<string> candidates = new List<string> { "candidate1", "candidate2" };
         var rand = new Random();
         for (int i = 0; i < amount; i++)
         {
-            bulleteins.Add(new Bulletein() { Id = ids[i], Message = candidates[rand.Next(0, 2)] });
+            bulleteins.Add(new Bulletein() { Id = tokens[i].Id, Message = candidates[rand.Next(0, 2)] });
         }
 
         List<byte[]> bytes = new List<byte[]>();
 
         foreach (var bulletein in bulleteins) 
         {
-            ElGamal.Encrypt(bulletein, keys[bulleteins.IndexOf(bulletein)]);
+            ElGamal.Encrypt(bulletein, tokens[bulleteins.IndexOf(bulletein)].Keys);
             bytes.Add(Bulletein.ObjectToByteArray(bulletein));
-            bBS.Encrypt(bytes[^1], bbsKeys.Item1);
+            bBS.Encrypt(bytes[^1], tokens[bulleteins.IndexOf(bulletein)].N);
         }
 
         for (int i = 0; i < bytes.Count; i++)
         {
-            var decr = bBS.Decrypt(bytes[i], bbsKeys.Item1);
+            var decr = bBS.Decrypt(bytes[i], tokens[i].N);
             bulleteins[i] = Bulletein.ByteArrayToObject(decr);
             ElGamal.Decrypt(bulleteins[i], keys[i]);
             if (!ids.Contains(bulleteins[i].Id))
---
+++ PAEG6/PAEG6/Token.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAEG6
+{
+    internal class Token
+    {
+        public Guid Id { get; set; }
+
+        public Keys Keys { get; set; }
+
+        public BigInteger N { get; set; }
+    }
+}
